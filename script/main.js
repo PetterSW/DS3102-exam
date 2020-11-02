b2ad26ccc2f133639b40324006e34608490d9227
PetterW <petter.wibstad@icloud.com>
2020-11-02 19:16:43 +0100

    +added UU text in CSS Grid

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
-import { Product, ProductListElement } from './product.js'
-import { Review, ReviewElement } from '../script/reviews.js'
-import { UUElement } from '../script/universell-utforming.js'
+import {UUElement} from '../script/universal-design.js';
+import { Product, ProductListElement } from '../script/product.js';
+import { Review, ReviewElement } from '../script/reviews.js';
+
+export {
+    UUElement, Product, ProductListElement, Review, ReviewElement
+}
