97353570e5954b062a0f09e1ff05dc2ce0375370
Magnus <magnus.oma@gmail.com>
2020-11-03 10:50:54 +0100

    changed import link order

--- script/main.js
+++ script/main.js
@@ -1,10 +1,9 @@
 
-import {UUElement} from '../script/universal-design.js'
-import { Product, ProductListElement } from '../script/product.js'
-import { Review, ReviewElement } from '../script/reviews.js'
-import { ShoppingCart } from '../script/shopping_cart.js'
+import {UUElement} from '../script/universal-design.js';
+import { Product, ProductListElement } from '../script/product.js';
+import { ShoppingCart } from '../script/shopping_cart.js';
+import { Review, ReviewElement } from '../script/reviews.js';
 
 export {
-    UUElement, Product, ProductListElement, Review, ReviewElement, ShoppingCart
+    UUElement, Product, ProductListElement, Review, ShoppingCart, ReviewElement
 }
-
