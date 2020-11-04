361a9c1d1cb25919bd57ee74f4bb203170a86aca
PetterW <petter.wibstad@icloud.com>
2020-11-04 20:05:35 +0100

    Slideshow, slides out, with a wrap around problem...

--- script/main.js
+++ script/main.js
@@ -18,39 +18,47 @@ export function shoppingCartQty() {
 /* Slideshow */
 
 let slideIndex = 1;
-showBanner(slideIndex);
 
-function currentBanner(index) {
-    showBanner (slideIndex = index);
-}
-
-function showBanner(index) {
+function showBanner(currentIndex, newIndex) {
+    console.log(currentIndex, newIndex);
     let slide = document.getElementsByClassName("slideshow");
     let img = document.getElementsByClassName("slide-img");
     
-    if (index > slide.length) {
+    if (newIndex > slide.length) {
         slideIndex = 1;
     }
-    if (index < 1) {
+    if (newIndex < 1) {
         slideIndex = slide.length;
     }
+
+    /*
     for(let i = 0; i < slide.length; i++) {
         img[i].style.transform = "translateX(-100%)";
         slide[i].style.display = "none";
     }
-    slide[slideIndex-1].style.display = "block";
+    */
+    slide[slideIndex-1].style = "height: auto; width: auto;";
+    slide[slideIndex-1].style.transform = "translateX(-100%)";
     anime({
-        targets: img[slideIndex-1],
+        targets: slide[slideIndex-1],
         translateX: '0%',
         easing: 'easeInOutExpo'
       });
+    anime({
+        targets: slide[currentIndex-1],
+        translateX: '100%',
+        easing: 'easeInOutExpo'
+      });
+    setTimeout(function () {
+        slide[currentIndex-1].style = "height: 0px; width: auto;";
+    }, 1000); 
 }
 
 document.getElementById("next-btn").addEventListener("click", () => {
-    showBanner(slideIndex += 1);
+    showBanner(slideIndex,slideIndex += 1);
     
 })
 
 document.getElementById("prev-btn").addEventListener("click", () => {
-    showBanner(slideIndex += -1);
+    showBanner(slideIndex,slideIndex += -1);
 })
