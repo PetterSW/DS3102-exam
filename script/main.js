6fad2db05241e5017a6bbc2a594eb7682183b725
PetterW <petter.wibstad@icloud.com>
2020-11-05 13:25:30 +0100

    Working slideshow in banner

--- script/main.js
+++ script/main.js
@@ -17,27 +17,30 @@ export function shoppingCartQty() {
 
 /* Slideshow */
 
+/**Set height of slideshow container*/
+let slideshowContainer = document.querySelector(".slideshow-container");
+let img = document.getElementsByClassName("slide-img");
+let imgHeight = getComputedStyle(img[0]).height;
+
+slideshowContainer.style.height = imgHeight;
+
 let slideIndex = 1;
 
-function showBanner(currentIndex, newIndex) {
-    console.log(currentIndex, newIndex);
+/* Change banner */
+
+function changeBanner(currentIndex, newIndex) {
     let slide = document.getElementsByClassName("slideshow");
-    let img = document.getElementsByClassName("slide-img");
     
+    /*If user clickes next on the last banner, start on new*/
     if (newIndex > slide.length) {
         slideIndex = 1;
     }
+    /*If user clickes previous on the first banner, */
     if (newIndex < 1) {
         slideIndex = slide.length;
     }
-
-    /*
-    for(let i = 0; i < slide.length; i++) {
-        img[i].style.transform = "translateX(-100%)";
-        slide[i].style.display = "none";
-    }
-    */
-    slide[slideIndex-1].style = "height: auto; width: auto;";
+    /*Animation for going backward */
+   if(currentIndex > slideIndex) {
     slide[slideIndex-1].style.transform = "translateX(-100%)";
     anime({
         targets: slide[slideIndex-1],
@@ -50,15 +53,36 @@ function showBanner(currentIndex, newIndex) {
         easing: 'easeInOutExpo'
       });
     setTimeout(function () {
-        slide[currentIndex-1].style = "height: 0px; width: auto;";
+        slide[currentIndex-1].style.transform = "translateX(100%)";
     }, 1000); 
+   }
+   /*Animation for going forward */
+   if(currentIndex < slideIndex) {
+    slide[slideIndex-1].style.transform = "translateX(100%)";
+    anime({
+        targets: slide[slideIndex-1],
+        translateX: '0%',
+        easing: 'easeInOutExpo'
+      });
+    anime({
+        targets: slide[currentIndex-1],
+        translateX: '-100%',
+        easing: 'easeInOutExpo'
+      });
+    setTimeout(function () {
+        slide[currentIndex-1].style.transform = "translateX(-100%)";
+    }, 1000); 
+   }
+    
 }
 
+/*Eventlistner for buttons on the banner*/
+
 document.getElementById("next-btn").addEventListener("click", () => {
-    showBanner(slideIndex,slideIndex += 1);
+    changeBanner(slideIndex,slideIndex += 1);
     
 })
 
 document.getElementById("prev-btn").addEventListener("click", () => {
-    showBanner(slideIndex,slideIndex += -1);
+    changeBanner(slideIndex,slideIndex += -1);
 })
